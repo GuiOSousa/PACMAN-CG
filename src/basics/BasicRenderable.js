617642b9273d3ef230f3723df0f29a4be01fbded
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-12-13 12:06:08 -0300

    Vercel Fix

--- src/basics/BasicRenderable.js
+++ src/basics/BasicRenderable.js
@@ -24,40 +24,85 @@ export default class Renderable {
     }
 
     basicDraw(program, uMVP, view, proj) {
-    const gl = this.gl;
+        const gl = this.gl;
 
-    const model = M4.multiply(
-        M4.translation(...this.position),
-        M4.yRotation(this.rotation)
-    );
+        const model = M4.multiply(
+            M4.translation(...this.position),
+            M4.yRotation(this.rotation)
+        );
 
-    gl.uniformMatrix4fv(
+        gl.uniformMatrix4fv(
+                gl.getUniformLocation(program, "uModel"),
+                false,
+                new Float32Array(model)
+        );
+
+        const viewModel = M4.multiply(view, model);
+        const mvp = M4.multiply(proj, viewModel);
+
+        gl.uniformMatrix4fv(uMVP, false, mvp);
+
+        const aPos = gl.getAttribLocation(program, "aPos");
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.enableVertexAttribArray(aPos);
+        gl.vertexAttribPointer(aPos, 3, gl.FLOAT, false, 0, 0);
+
+        const aColor = gl.getAttribLocation(program, "aColor");
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+        gl.enableVertexAttribArray(aColor);
+        gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+
+        gl.uniform1i(gl.getUniformLocation(program, "uHasTexture"), 0);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+        gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
+        }
+
+    textureDraw(program, uMVP, view, proj) {
+        const gl = this.gl;
+        if (!this.texture) return;
+
+        const model = M4.multiply(
+            M4.translation(...this.position),
+            M4.yRotation(this.rotation)
+        );
+
+        // uModel
+        gl.uniformMatrix4fv(
             gl.getUniformLocation(program, "uModel"),
             false,
             new Float32Array(model)
-    );
+        );
 
-    const viewModel = M4.multiply(view, model);
-    const mvp = M4.multiply(proj, viewModel);
+        // MVP
+        const viewModel = M4.multiply(view, model);
+        const mvp = M4.multiply(proj, viewModel);
+        gl.uniformMatrix4fv(uMVP, false, mvp);
 
-    gl.uniformMatrix4fv(uMVP, false, mvp);
+        // Texture active
+        gl.uniform1i(gl.getUniformLocation(program, "uHasTexture"), 1);
 
-    const aPos = gl.getAttribLocation(program, "aPos");
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-    gl.enableVertexAttribArray(aPos);
-    gl.vertexAttribPointer(aPos, 3, gl.FLOAT, false, 0, 0);
+        // Position
+        const aPos = gl.getAttribLocation(program, "aPos");
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.enableVertexAttribArray(aPos);
+        gl.vertexAttribPointer(aPos, 3, gl.FLOAT, false, 0, 0);
 
-    const aColor = gl.getAttribLocation(program, "aColor");
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
-    gl.enableVertexAttribArray(aColor);
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+        // UV
+        const aUV = gl.getAttribLocation(program, "aUV");
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uvBuffer);
+        gl.enableVertexAttribArray(aUV);
+        gl.vertexAttribPointer(aUV, 2, gl.FLOAT, false, 0, 0);
 
-    gl.uniform1i(gl.getUniformLocation(program, "uHasTexture"), 0);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+        // Texture
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.uniform1i(gl.getUniformLocation(program, "uTexture"), 0);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-
-    gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
-}
-
+        // Indexes
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
+    }
 }
